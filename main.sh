b25c24be1f8c706ef14c3d456d2eab4d2e03f97f
Its Sofiyan! <147476847+Sofiyan1-786@users.noreply.github.com>
2024-05-23 21:25:43 -0700

    UPDATED

--- main.sh
+++ main.sh
@@ -3,13 +3,13 @@ urls=("http://utagechildcare.com" "http://unicusdiagnostics.com")
 
 for url in "${urls[@]}"; do
 
-    wget -O temp.html "$url"
-    grep_output=$(grep -o '<img[^>]*src="[^"]*"' temp.html)
+    wget -O Arbitrary.html "$url"
+    grep_output=$(grep -o '<img[^>]*src="[^"]*"' Arbitrary.html)
 
     for img_tag in $grep_output; do
         image_url=$(echo "$img_tag" | sed 's/<img[^>]*src="//')
         image_url=$(echo "$image_url" | sed 's/".*//') 
         wget "$image_url"
     done
-    rm temp.html
+    rm Arbitrary.html
 done
