43035acbdf7c9e6e4b651c0a5240aa69461cb9e5
Its Sofiyan! <147476847+Sofiyan1-786@users.noreply.github.com>
2024-05-23 21:36:52 -0700

    FINAL COMMIT

--- main.sh
+++ main.sh
@@ -2,14 +2,15 @@
 urls=("http://utagechildcare.com" "http://unicusdiagnostics.com")
 
 for url in "${urls[@]}"; do
-
-    wget -O Arbitrary.html "$url"
+    wget -O Arbitrary.html "$url" &
+    pid1=$!
     grep_output=$(grep -o '<img[^>]*src="[^"]*"' Arbitrary.html)
 
     for img_tag in $grep_output; do
         image_url=$(echo "$img_tag" | sed 's/<img[^>]*src="//')
         image_url=$(echo "$image_url" | sed 's/".*//') 
-        wget "$image_url"
+        wget "$image_url" &
     done
-    rm Arbitrary.html
+    wait $pid1
+    rm .html
 done
