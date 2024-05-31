6ea2ebf85ec878ff3a42e56c2c26e28a6cedeee2
Its Sofiyan! <147476847+Sofiyan1-786@users.noreply.github.com>
2024-05-31 16:05:19 -0700

    Updated main.sh

--- main.sh
+++ main.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
+
 urls=("http://utagechildcare.com" "http://unicusdiagnostics.com")
-max_jobs=10
-counter=0 #jobs CURRENTLY.
+max_jobs=1000
+counter=0 # Counter for the number of jobs currently running.
+
 for url in "${urls[@]}"; do
     wget -O Arbitrary.html "$url" &>/dev/null
     grep_output=$(grep -o '<img[^>]*src="[^"]*"' Arbitrary.html)
     for img_tag in $grep_output; do
         temp_string=${img_tag#*src=\"}
         img_url=${temp_string%%\"*}
-        img_name=$(basename "$img_url")
         echo "Found pic at: $img_url"
-        if [ ! -f "$img_name" ]; then # ADDITION MADE HERE FOR THE EXISTENSE OF THE FILE.
-            ((counter=counter%max_jobs)) # CHECKING WEATHER THE COUNTER IS GREATER THAN THE JOB MAX.
-            ((counter++==0)) && wait # Corrected cc management.
-            echo "File $img_name DNE - downloading it..."
+        img_name=$(basename "$img_url")
+        if [ ! -f "$img_name" ]; then
+            #if it doesnt exists then it downloads
             wget "$img_url" -O "$img_name" &>/dev/null &
-            echo "SUCCESSFULLY DOWNLOADED THE IMAGE $img_name!" 
+            echo "File $img_name DNE - downloading it..."
+            ((counter++))
+            #ADDITION FROM HERE MADE HERE: Wait for jobs to complete if max_jobs limit is reached
+            if (( counter >= max_jobs )); then
+                wait
+                counter=0
+            fi
+            echo "SUCCESSFULLY DOWNLOADED THE IMAGE $img_name!"
         else
             echo "$img_name Exists, skipping..."
         fi
