25d6875cf07d5da4e139a853f5835565bac52815
Its Sofiyan! <147476847+Sofiyan1-786@users.noreply.github.com>
2024-06-15 21:19:07 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -47,35 +47,4 @@ done
 wait
 echo "SCRAPING HAS BEEN COMPLETED."
 exit
-
-############################
-## OLD SCRIPT FOR REFERENCE
-############################
-for url in "${urls[@]}"; do
-    wget -O Arbitrary.html "$url" &>/dev/null
-    grep_output=$(grep -o '<img[^>]*src="[^"]*"' Arbitrary.html)
-    for img_tag in $grep_output; do
-        temp_string=${img_tag#*src=\"}
-        img_url=${temp_string%%\"*}
-        echo "Found pic at: $img_url"
-        img_name=$(basename "$img_url")
-        if [ ! -f "$img_name" ]; then
-            #if it doesnt exists then it downloads
-            wget "$img_url" -O "$img_name" &>/dev/null &
-            echo "File $img_name DNE - downloading it..."
-            ((counter++))
-            #FEW ADDITION FROM HERE MADE HERE: Wait for jobs to complete if max_jobs limit is reached
-            if (( counter >= max_jobs )); then
-                  wait -n
-                 ((counter--))
-            fi
-            echo "SUCCESSFULLY DOWNLOADED THE IMAGE $img_name!"
-        else
-            echo "$img_name Exists, skipping..."
-        fi
-    done
-    rm Arbitrary.html
-done
-wait
-echo "SCRAPING HAS BEEN COMPLETED."
-
+#EARLIER I DIDNT KNEW IF MULTIPLE PROCESSES CAN BE COMPLETED PARALLELY:) NOW FIXED
